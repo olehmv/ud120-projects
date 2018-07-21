89fda21cb952316db531835b5d3145308034e5bf
olehmv <oleg19850405@hotmail.com>
2018-07-21 14:45:52 +0300

    Decision Tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -23,7 +23,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+
+print "number of featers", len(features_train[0])
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+### fit the classifier on the training features and labels
+t0 = time()
+clf.fit(features_train, labels_train)### training time: 54.22 s
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)### predict time: 0.031 s
+print "predict time:", round(time()-t0, 3), "s"
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred,labels_test)
+print accuracy ### 0.9766780432309442
+
 
 
 #########################################################
